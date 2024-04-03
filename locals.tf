712b08f6f7a809e11f44cdceab100952d95378f9
john.knutson@hennepin.us <john.knutson@hennepin.us>
2024-04-03 11:30:32 -0500

    hack: override policy assignments

--- locals.tf
+++ locals.tf
@@ -1,7 +1,9 @@
 # Jsondecode the data source but use known (at plan time) map keys from `alz_archetype_keys`
 # and combine with (potentially) known after apply data from the `alz_archetype` data source.
 locals {
-  alz_policy_assignments_decoded     = { for k in data.alz_archetype_keys.this.alz_policy_assignment_keys : k => jsondecode(data.alz_archetype.this.alz_policy_assignments[k]) }
+  # alz_policy_assignments_decoded     = { for k in data.alz_archetype_keys.this.alz_policy_assignment_keys : k => jsondecode(data.alz_archetype.this.alz_policy_assignments[k]) }
+  # alz_policy_assignments_decoded     = { for k in data.alz_archetype_keys.this.alz_policy_assignment_keys : k => {} }
+  alz_policy_assignments_decoded     = {}
   alz_policy_definitions_decoded     = { for k in data.alz_archetype_keys.this.alz_policy_definition_keys : k => jsondecode(data.alz_archetype.this.alz_policy_definitions[k]) }
   alz_policy_set_definitions_decoded = { for k in data.alz_archetype_keys.this.alz_policy_set_definition_keys : k => jsondecode(data.alz_archetype.this.alz_policy_set_definitions[k]) }
   alz_role_definitions_decoded       = { for k in data.alz_archetype_keys.this.alz_role_definition_keys : k => jsondecode(data.alz_archetype.this.alz_role_definitions[k]) }
@@ -9,13 +11,14 @@ locals {
 
 # Create a map of role assignment for the scope of the management group
 locals {
-  policy_role_assignments = data.alz_archetype.this.alz_policy_role_assignments != null ? {
-    for pra_key, pra_val in data.alz_archetype.this.alz_policy_role_assignments : pra_key => {
-      scope              = pra_val.scope
-      role_definition_id = pra_val.role_definition_id
-      principal_id       = one(azurerm_management_group_policy_assignment.this[pra_val.assignment_name].identity).principal_id
-    }
-  } : {}
+  policy_role_assignments = {}
+  # policy_role_assignments = data.alz_archetype.this.alz_policy_role_assignments != null ? {
+  #   for pra_key, pra_val in data.alz_archetype.this.alz_policy_role_assignments : pra_key => {
+  #     scope              = pra_val.scope
+  #     role_definition_id = pra_val.role_definition_id
+  #     principal_id       = one(azurerm_management_group_policy_assignment.this[pra_val.assignment_name].identity).principal_id
+  #   }
+  # } : {}
 }
 
 # Get parent management group name from the parent_id
